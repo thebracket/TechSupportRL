a03baac03066e91d5a162453be0568aae64de44c
Herbert Wolverson <herberticus@gmail.com>
2016-11-14 09:26:18 -0600

    RLTK Hello World with layers

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,20 @@
 using namespace rltk;
 
 void tick(double duration_ms) {
-	console->clear();
-    console->print(1,1, "Hello World");
+	term(2)->clear();
+    term(2)->print(1,1, "Hello World");
+}
+
+void resize_map(layer_t * l, int w, int h) {
+	// Use the whole window
+	l->w = w;
+	l->h = h;
 }
 
 int main() {
-    init(config_simple("../assets", 80, 50, "RLTK Hello World", "8x8"));
+    init(config_advanced("../assets", 1020, 768, "Tech Support - The Roguelike", false));
+    gui->add_layer(1, 0, 0, 1024, 768, "16x16", resize_map);
+    gui->add_layer(2, 0, 0, 1024, 768, "8x16", resize_map);
     run(tick);
     return 0;
 }
